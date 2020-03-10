7d0d592ef1aaeebc68bdee0d58d24296b3127eed
vera graha <44109137+veragraha@users.noreply.github.com>
2020-03-10 09:07:44 +0700

    uploadpertama
    uploadpertama

--- application/views/login.php
+++ application/views/login.php
@@ -1,45 +1,54 @@
-
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 </head>
-<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css">
 <body>
 
+<div class="container">
+	<center>
+		<div class="col-md-5 col-md-offset-3">
+			<div class="alert alert-danger">
+				<div class="panel-heading">
+					<h2>
+						<Toko><span class="glyphicon glyphicon-cutlery" aria hidden="true"></span>  login Restoran Vera
+					</h2>
+					<p><span class="glypicon glypicon" aria-hidden="true"></span></h3>
+					<h3>Login System</h3>
+					<p><span class="glyphicon glyphicon-road" aria-hidden="true"></span> jln.Bangsri-jepara</p>
+					
+				</div>
+				<div class="panel-body">
+					<div class="alert alert-success">
+						<span class="glypicon glypicon-exlamation-sign" aria-hidden="true"></span>
+						masukkan Username dengan Benar!
+					</div>
+					
+				</div>
+				<div class="panel-body">
+					<div class="col-md-11">	
+						<div method="post">	
+							<div class="input group">
+								<span class="input_group-addon1">Username</span>
+								<input type="text" name="user" class="form-control" placeholder="Username"aria-descridby="basic-addon1"required="required">
+							</div>
+							<br>
+							<div class="input group">
+								<span class="input_group-addon1">Password</span>
+								<input type="text" name="user" class="form-control" placeholder="Password"aria-descridby="basic-addon1"required="required">	
+							</div>
+							<br>	
+					</div>
+					<input type="submit" class="btn-btn-block btn-danger" value="login">
+				</div>
+				
+			</div>
+		</div>
+	</center>
 
-    <div class="container">
-      <center>
-      <div class="col-md-5 col-md-offset-3">
-        <div class="panel panel-warning">
-          <div class="panel-heading">
-            <h3> <span class="glyphicon glyphicon-book" aria-hidden="true"></span> eresto</h3>
-            <h4>Login System</h4>
-            <p> <span class="glyphicon glyphicon-road" aria-hidden="true"></span> Jln.Bangsri-Jepara </p>
-    
-              </div>
-                <div class="panel-body">
 
-      
-                  <div class="col-md-11">
-
-                  <form method="post">
-
-
-                    <div class="input-group">
-                    <span class="input-group-addon" id="basic-addon1">Username</span>
-                    <input type="text" name="user" class="form-control" placeholder="Username" aria-describedby="basic-addon1" required="required">
-                  </div>
-                  <br>
-                  <div class="input-group">
-                    <span class="input-group-addon" id="basic-addon1">Password</span>
-                    <input type="password" name="pass" class="form-control" placeholder="Password" aria-describedby="basic-addon1" required="required">
-                  </div>
-                  <br>
-                  </div>
-                    <input type="submit" class="btn btn-block btn-warning" value="Login">
-                  
-                  </form>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
 
 </body>
 </html>
