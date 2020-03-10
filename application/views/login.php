c5fe58e50acd4b82c9197f6127d8d2d90d4ed200
vera graha <44109137+veragraha@users.noreply.github.com>
2020-03-10 08:56:46 +0700

    Initial commit

---
+++ application/views/login.php
@@ -0,0 +1,45 @@
+
+</head>
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css">
+<body>
+
+
+    <div class="container">
+      <center>
+      <div class="col-md-5 col-md-offset-3">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h3> <span class="glyphicon glyphicon-book" aria-hidden="true"></span> eresto</h3>
+            <h4>Login System</h4>
+            <p> <span class="glyphicon glyphicon-road" aria-hidden="true"></span> Jln.Bangsri-Jepara </p>
+    
+              </div>
+                <div class="panel-body">
+
+      
+                  <div class="col-md-11">
+
+                  <form method="post">
+
+
+                    <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon1">Username</span>
+                    <input type="text" name="user" class="form-control" placeholder="Username" aria-describedby="basic-addon1" required="required">
+                  </div>
+                  <br>
+                  <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon1">Password</span>
+                    <input type="password" name="pass" class="form-control" placeholder="Password" aria-describedby="basic-addon1" required="required">
+                  </div>
+                  <br>
+                  </div>
+                    <input type="submit" class="btn btn-block btn-warning" value="Login">
+                  
+                  </form>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
+</body>
+</html>
